e7ac7ef2bcc948588ea09fb89985a3c912477c31
kmagdi <katay.magdi@gmail.com>
2020-09-22 18:59:33 +0200

    homepage settings

---
+++ build/precache-manifest.0315e2aa4d235969d2ee829cc18cab28.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f12c40e56e3a3aa6eec502db88df7d",
+    "url": "/kando/index.html"
+  },
+  {
+    "revision": "eda549682f256ddb0ab7",
+    "url": "/kando/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b4d3138e4ba3c324708f",
+    "url": "/kando/static/css/main.159d9cce.chunk.css"
+  },
+  {
+    "revision": "eda549682f256ddb0ab7",
+    "url": "/kando/static/js/2.a6cdd82d.chunk.js"
+  },
+  {
+    "revision": "a3d5da51d8489a02931a36aa8a33f4e5",
+    "url": "/kando/static/js/2.a6cdd82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d3138e4ba3c324708f",
+    "url": "/kando/static/js/main.f6d04a5b.chunk.js"
+  },
+  {
+    "revision": "e9e5dcbd121cf1a3d15b",
+    "url": "/kando/static/js/runtime-main.ba150669.js"
+  },
+  {
+    "revision": "5e95016b2efe05050caa5e27fc7e1372",
+    "url": "/kando/static/media/SZ2.5e95016b.jpg"
+  },
+  {
+    "revision": "d86c547b80999c201be1240787f01272",
+    "url": "/kando/static/media/T1.d86c547b.jpg"
+  },
+  {
+    "revision": "f91c98cf2d8acc30f4594b7fbeccb2a0",
+    "url": "/kando/static/media/T10.f91c98cf.jpg"
+  },
+  {
+    "revision": "97c8930fb5b7bd582b3fa5b1ec5deffc",
+    "url": "/kando/static/media/T11.97c8930f.jpg"
+  },
+  {
+    "revision": "e836d712ab61690baf9af28831a90909",
+    "url": "/kando/static/media/T12.e836d712.jpg"
+  },
+  {
+    "revision": "ba872270d9a05b6614e2c128cf162248",
+    "url": "/kando/static/media/T13.ba872270.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/T14-1.908ec899.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/T14-2.908ec899.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/T14-3.908ec899.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/T14-4.908ec899.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/T14-5.908ec899.jpg"
+  },
+  {
+    "revision": "f73c62d3da90279ed8f3c053e6cccedc",
+    "url": "/kando/static/media/T14.f73c62d3.jpg"
+  },
+  {
+    "revision": "bd6210edbae984aa26c771bd0f4f9fe1",
+    "url": "/kando/static/media/T2.bd6210ed.jpg"
+  },
+  {
+    "revision": "fe2ce265052f18bde95db4a185887be6",
+    "url": "/kando/static/media/T3.fe2ce265.jpg"
+  },
+  {
+    "revision": "ae6c0c9c308f1faa50ea53a1c669c1d6",
+    "url": "/kando/static/media/T4.ae6c0c9c.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/T5.908ec899.jpg"
+  },
+  {
+    "revision": "182cb864d6923132193d9c7f139313a2",
+    "url": "/kando/static/media/T6.182cb864.jpg"
+  },
+  {
+    "revision": "b050090a0c4e89c3dc76d8876a401fe4",
+    "url": "/kando/static/media/T7.b050090a.jpg"
+  },
+  {
+    "revision": "01663e46e6f5f756e4a67853f27cab03",
+    "url": "/kando/static/media/T8.01663e46.jpg"
+  },
+  {
+    "revision": "87965fa536e8041da82f0b4c2c7b41de",
+    "url": "/kando/static/media/T9.87965fa5.jpg"
+  },
+  {
+    "revision": "15dce6bd7b1c39086bdb5ec9e8f6ebab",
+    "url": "/kando/static/media/eszt.15dce6bd.png"
+  },
+  {
+    "revision": "1ea094f5e3668af278840519631b786f",
+    "url": "/kando/static/media/eszt2.1ea094f5.jpg"
+  },
+  {
+    "revision": "c6324cc4fb450c7ea81180458f49d10a",
+    "url": "/kando/static/media/fade.c6324cc4.png"
+  },
+  {
+    "revision": "fbc229fc29cfe232ffaba03b9186c8fb",
+    "url": "/kando/static/media/footer.fbc229fc.png"
+  },
+  {
+    "revision": "36f0d0968b1bd61d617803bead53ebf0",
+    "url": "/kando/static/media/gyakorlat.36f0d096.jpg"
+  },
+  {
+    "revision": "44973c4de9eec95c40ffbb9e10c239d9",
+    "url": "/kando/static/media/gyakorlat.44973c4d.jpg"
+  },
+  {
+    "revision": "665108413d67e1af3c37acfa62dbe972",
+    "url": "/kando/static/media/gyakorlat.66510841.jpg"
+  },
+  {
+    "revision": "7ff22e1be8c111e810838461524134ae",
+    "url": "/kando/static/media/head.7ff22e1b.jpg"
+  },
+  {
+    "revision": "863333ad49b6673daba7c14664a3bb8f",
+    "url": "/kando/static/media/head.863333ad.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/head.908ec899.jpg"
+  },
+  {
+    "revision": "f1ba8ceb4cdecdb5ba0b312cb8ce85ef",
+    "url": "/kando/static/media/head.f1ba8ceb.jpg"
+  },
+  {
+    "revision": "33334aa8a2f4cd2c13add428e40c4d54",
+    "url": "/kando/static/media/ismeretek.33334aa8.jpg"
+  },
+  {
+    "revision": "3cbdd993d2f50b352ca82a75da42d48c",
+    "url": "/kando/static/media/ismeretek.3cbdd993.jpg"
+  },
+  {
+    "revision": "9f70f481010975fbb1bd8b81b21e4130",
+    "url": "/kando/static/media/ismeretek.9f70f481.jpg"
+  },
+  {
+    "revision": "0737fbef9cdc89d5d75c9ac4168b8f39",
+    "url": "/kando/static/media/kinek.0737fbef.jpg"
+  },
+  {
+    "revision": "295ba3764fddc3dba4044eb1d1280c45",
+    "url": "/kando/static/media/kinek.295ba376.jpg"
+  },
+  {
+    "revision": "ef1327cc0e657d9536dec64a53790dc6",
+    "url": "/kando/static/media/kinek.ef1327cc.jpg"
+  },
+  {
+    "revision": "099ab748a42195d3cdb1753ceaf91045",
+    "url": "/kando/static/media/logo.099ab748.png"
+  },
+  {
+    "revision": "4cc688469261ecb10b5739c4dd191ecc",
+    "url": "/kando/static/media/mitcsinal.4cc68846.jpg"
+  },
+  {
+    "revision": "5371ea85044ba63951a3c0fe37deb185",
+    "url": "/kando/static/media/mitcsinal.5371ea85.jpg"
+  },
+  {
+    "revision": "9fd854a02e9489cb53e1595c8e5b2401",
+    "url": "/kando/static/media/mitcsinal.9fd854a0.jpg"
+  },
+  {
+    "revision": "3ea1926dfd80070c7821261327363546",
+    "url": "/kando/static/media/munkakor.3ea1926d.jpg"
+  },
+  {
+    "revision": "55b86df1b39a5e0330a33b793485928b",
+    "url": "/kando/static/media/munkakor.55b86df1.jpg"
+  },
+  {
+    "revision": "b447b3aa196be7a685402b4d5e285464",
+    "url": "/kando/static/media/munkakor.b447b3aa.jpg"
+  },
+  {
+    "revision": "39678937678ed7b037084591130ef4b7",
+    "url": "/kando/static/media/parallax1.39678937.jpg"
+  },
+  {
+    "revision": "7bad7113448629c3210cf51e9d64f8e2",
+    "url": "/kando/static/media/parallax1.7bad7113.jpg"
+  },
+  {
+    "revision": "908ec899be58e1465f69ce54ca809a16",
+    "url": "/kando/static/media/parallax1.908ec899.jpg"
+  },
+  {
+    "revision": "8a40f754f7f53c28ba5c7c4b4cdd7fc7",
+    "url": "/kando/static/media/parallax2.8a40f754.jpg"
+  },
+  {
+    "revision": "8ad503483e663b2a98565c8705bcdced",
+    "url": "/kando/static/media/parallax2.8ad50348.jpg"
+  },
+  {
+    "revision": "b7d43887b423f4d026d65e035aa4c075",
+    "url": "/kando/static/media/parallax2.b7d43887.jpg"
+  }
+]);
