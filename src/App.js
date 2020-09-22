25262f120ac6e67b6bcbbb43a7df5ce5aa382fa7
kmagdi <katay.magdi@gmail.com>
2020-09-22 20:14:12 +0200

    adding basename

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import { ParallaxProvider } from 'react-scroll-parallax'
 import './App.css'
 
 export const App=()=>{
+console.log("This is the process.env"+process.env.PUBLIC_URL)
     return(
+        
         <ParallaxProvider>
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <MyNavbar/>
                 <Switch>
                     <Route exact path="/" component={Home}/>
